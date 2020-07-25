d54ab2a8bffd9abf813d0be6b333d618bdf30bee
roctbb <roctbb@gmail.com>
2020-07-25 14:40:17 +0300

    working on

--- goto_stonks/settings.py
+++ goto_stonks/settings.py
@@ -111,7 +111,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 PROJECT_ROOT = os.path.normpath(os.path.dirname(__file__))
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'goto_stonks/static')
 STATIC_URL = '/static/'
 
 mimetypes.add_type("text/css", ".css", True)
